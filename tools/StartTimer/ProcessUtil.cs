595fc2a04b0fe2f2658ed2d4b094711b84c0e45d
Damian Edwards <damian@damianedwards.com>
2023-05-10 18:33:01 -0700

    Add StartTimer tool for measuring app startup time

---
+++ tools/StartTimer/ProcessUtil.cs
@@ -0,0 +1,246 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace StartTimer;
+
+public static class ProcessUtil
+{
+    #region Native Methods
+
+    [DllImport("libc", SetLastError = true, EntryPoint = "kill")]
+    private static extern int sys_kill(int pid, int sig);
+
+    #endregion
+
+    private static readonly bool IsWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+    private const int ProcessExitTimeoutMs = 30 * 1000; // 30 seconds timeout for the process to exit.
+
+    public static async Task<ProcessResult> RunAsync(
+        string filename,
+        string arguments,
+        string? workingDirectory = null,
+        bool throwOnError = true,
+        IDictionary<string, string>? environmentVariables = null,
+        Action<string>? outputDataReceived = null,
+        Action<string>? errorDataReceived = null,
+        Action<int>? onStart = null,
+        Action<int>? onStop = null,
+        CancellationToken cancellationToken = default)
+    {
+        using var process = new Process()
+        {
+            StartInfo =
+            {
+                FileName = filename,
+                Arguments = arguments,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = !IsWindows,
+                WindowStyle = ProcessWindowStyle.Hidden
+            },
+            EnableRaisingEvents = true
+        };
+
+        if (workingDirectory != null)
+        {
+            process.StartInfo.WorkingDirectory = workingDirectory;
+        }
+
+        if (environmentVariables != null)
+        {
+            foreach (var kvp in environmentVariables)
+            {
+                process.StartInfo.Environment.Add(kvp!);
+            }
+        }
+
+        var outputLock = new SpinLock();
+
+        void WithOutputLock(Action action)
+        {
+            bool gotLock = false;
+
+            try
+            {
+                outputLock.Enter(ref gotLock);
+
+                action();
+            }
+            finally
+            {
+                if (gotLock)
+                {
+                    outputLock.Exit();
+                }
+            }
+        }
+
+        var outputBuilder = new StringBuilder();
+        process.OutputDataReceived += (_, e) =>
+        {
+            if (e.Data == null)
+            {
+                return;
+            }
+
+            if (outputDataReceived != null)
+            {
+                outputDataReceived.Invoke(e.Data);
+            }
+            else
+            {
+                WithOutputLock(() => outputBuilder.AppendLine(e.Data));
+            }
+        };
+
+        var errorBuilder = new StringBuilder();
+        process.ErrorDataReceived += (_, e) =>
+        {
+            if (e.Data == null)
+            {
+                return;
+            }
+
+            if (errorDataReceived != null)
+            {
+                errorDataReceived.Invoke(e.Data);
+            }
+            else
+            {
+                WithOutputLock(() => errorBuilder.AppendLine(e.Data));
+            }
+        };
+
+        var processLifetimeTask = new TaskCompletionSource<ProcessResult>();
+
+        process.Exited += (_, e) =>
+        {
+            lock (process)
+            {
+                // Even though the Exited event has been raised, WaitForExit() must still be called to ensure the output buffers
+                // have been flushed before the process is considered completely done.
+                // Because of the bug in the dotnet runtime https://github.com/dotnet/runtime/issues/29232, Process.WaitForExit()
+                // hangs for processes that spawn another long-running processes.
+                // Since these are expected to be long running processes and we're typically not concerned with capturing all of its output
+                // i.e. it's probably ok for some output to be lost on shutdown, since Tye is shutting down anyway,
+                // we call Process.WaitForProcessExit(ProcessExitTimeoutMs).
+                // Also, since this is a process.Exited event, process.ExitCode is valid even if WaitForExit() times out.
+                process.WaitForExit(ProcessExitTimeoutMs);
+            }
+
+            // NOTE: If WaitForExit() returns false, more output may be written,
+            //       so we must synchronize access to the output StringBuilders.
+
+            WithOutputLock(
+                () =>
+                {
+                    if (throwOnError && process.ExitCode != 0)
+                    {
+                        processLifetimeTask.TrySetException(new InvalidOperationException($"Command {filename} {arguments} returned exit code {process.ExitCode}. Standard error: \"{errorBuilder.ToString()}\""));
+                    }
+                    else
+                    {
+                        processLifetimeTask.TrySetResult(new ProcessResult(outputBuilder.ToString(), errorBuilder.ToString(), process.ExitCode));
+                    }
+                });
+        };
+
+        // lock ensures we're reading output when WaitForExit is called in process.Exited event.
+        lock (process)
+        {
+            process.Start();
+            onStart?.Invoke(process.Id);
+
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+        }
+
+        var cancelledTcs = new TaskCompletionSource<object?>();
+        await using var _ = cancellationToken.Register(() => cancelledTcs.TrySetResult(null));
+
+        var result = await Task.WhenAny(processLifetimeTask.Task, cancelledTcs.Task);
+
+        if (result == cancelledTcs.Task)
+        {
+            if (!IsWindows)
+            {
+                sys_kill(process.Id, sig: 2); // SIGINT
+            }
+            else
+            {
+                if (!process.CloseMainWindow())
+                {
+                    process.Kill(entireProcessTree: true);
+                }
+            }
+
+            if (!process.HasExited)
+            {
+                var cancel = new CancellationTokenSource();
+                await Task.WhenAny(processLifetimeTask.Task, Task.Delay(TimeSpan.FromSeconds(5), cancel.Token));
+                cancel.Cancel();
+
+                if (!process.HasExited)
+                {
+                    process.Kill(entireProcessTree: true);
+                }
+            }
+        }
+
+        var processResult = await processLifetimeTask.Task;
+        onStop?.Invoke(processResult.ExitCode);
+        return processResult;
+    }
+
+    public static Task<ProcessResult> RunAsync(ProcessSpec processSpec, CancellationToken cancellationToken = default, bool throwOnError = true)
+    {
+        return RunAsync(processSpec.Executable!, processSpec.Arguments!, processSpec.WorkingDirectory, throwOnError: throwOnError, processSpec.EnvironmentVariables, processSpec.OutputData, processSpec.ErrorData, processSpec.OnStart, processSpec.OnStop, cancellationToken);
+    }
+
+    public static void KillProcess(int pid)
+    {
+        try
+        {
+            using var process = Process.GetProcessById(pid);
+            process?.Kill(entireProcessTree: true);
+        }
+        catch (ArgumentException) { }
+        catch (InvalidOperationException) { }
+    }
+}
+
+public class ProcessResult
+{
+    public ProcessResult(string standardOutput, string standardError, int exitCode)
+    {
+        StandardOutput = standardOutput;
+        StandardError = standardError;
+        ExitCode = exitCode;
+    }
+
+    public string StandardOutput { get; }
+    public string StandardError { get; }
+    public int ExitCode { get; }
+}
+
+public class ProcessSpec
+{
+    public string? Executable { get; set; }
+    public string? WorkingDirectory { get; set; }
+    public IDictionary<string, string> EnvironmentVariables { get; set; } = new Dictionary<string, string>();
+    public string? Arguments { get; set; }
+    public Action<string>? OutputData { get; set; }
+    public Func<Task<int>>? Build { get; set; }
+    public Action<string>? ErrorData { get; set; }
+    public Action<int>? OnStart { get; set; }
+    public Action<int>? OnStop { get; set; }
+    public string? ShortDisplayName()
+        => Path.GetFileNameWithoutExtension(Executable);
+}
---
+++ tools/StartTimer/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Diagnostics;
+using StartTimer;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Missing required argument [path]");
+    return 1;
+}
+
+var target = args[0];
+
+var targetArgs = "";
+var sepIndex = Array.IndexOf(args, "--");
+if (sepIndex >= 0)
+{
+    var startFrom = sepIndex + 1;
+    targetArgs = string.Join(" ", args[startFrom..]);
+}
+
+TimeSpan started = default;
+TimeSpan firstOutput = default;
+TimeSpan readyToServe = default ;
+TimeSpan afterFirstReuqest = default;
+var listeningOnUrl = "";
+var pid = -1;
+var sendRequestTask = Task.CompletedTask;
+
+var sw = Stopwatch.StartNew();
+
+try
+{
+    var result = await ProcessUtil.RunAsync(target, targetArgs, Environment.CurrentDirectory, onStart: OnStart, outputDataReceived: OnOutput, errorDataReceived: OnError);
+}
+catch (Exception ex) when (pid == -1)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(ex.Message.ToString());
+    Console.ResetColor();
+    return 1;
+}
+catch (Exception)
+{ }
+
+await sendRequestTask;
+
+Console.WriteLine();
+Console.WriteLine($"Target executable '{target}' ran successfully");
+Console.WriteLine($"Started after {started.TotalMilliseconds:N2} ms");
+if (firstOutput != default)
+{
+    Console.WriteLine($"First output after {firstOutput.TotalMilliseconds:N2} ms");
+}
+if (readyToServe != default)
+{
+    Console.WriteLine($"Ready to accept requests after {readyToServe.TotalMilliseconds:N2} ms");
+}
+if (afterFirstReuqest != default)
+{
+    Console.WriteLine($"First request completed after {afterFirstReuqest.TotalMilliseconds:N2} ms");
+}
+
+return 0;
+
+void OnStart(int processId)
+{
+    started = sw.Elapsed;
+    pid = processId;
+}
+
+void OnOutput(string output)
+{
+    if (firstOutput == default)
+    {
+        firstOutput = sw.Elapsed;
+    }
+
+    if (output.Contains("Now listening on: http://"))
+    {
+        var schemeIndex = output.IndexOf("http://");
+        listeningOnUrl = output[schemeIndex..];
+    }
+    if (output.Contains("Application started. Press Ctrl+C to shut down."))
+    {
+        // App is ready to start
+        readyToServe = sw.Elapsed;
+
+        // Send request
+        sendRequestTask = SendRequestAndStopProcess();
+    }
+    Console.WriteLine(output);
+}
+
+void OnError(string error)
+{
+    if (firstOutput == default)
+    {
+        firstOutput = sw.Elapsed;
+    }
+
+    Console.WriteLine(error);
+}
+
+async Task SendRequestAndStopProcess()
+{
+    using var httpClient = new HttpClient();
+    var response = await httpClient.GetAsync(listeningOnUrl);
+    
+    afterFirstReuqest = sw.Elapsed;
+    ProcessUtil.KillProcess(pid);
+}
