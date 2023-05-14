c1b4eab5d73713b7c9146756bd607fffda1c69a3
David Fowler <davidfowl@gmail.com>
2023-05-13 20:25:02 -0700

    Single line

--- src/Frontend/YarpExtensions.cs
+++ src/Frontend/YarpExtensions.cs
@@ -10,10 +10,8 @@ public static class YarpExtensions
     public static IEndpointConventionBuilder MapForwarder(this IEndpointRouteBuilder routes, string pattern, string destinationPrefix, string targetPath)
     {
         var transformBuilder = routes.ServiceProvider.GetRequiredService<ITransformBuilder>();
-        var transform = transformBuilder.Create(c =>
-        {
-            c.AddPathRouteValues(targetPath);
-        });
+
+        var transform = transformBuilder.Create(c => c.AddPathRouteValues(targetPath));
 
         return routes.MapForwarder(pattern, destinationPrefix, new ForwarderRequestConfig(), transform);
     }
