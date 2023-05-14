c60e32ce486ae5e6e65ed7220fbe4cafc497850f
David Fowler <davidfowl@gmail.com>
2023-05-14 06:21:00 -0700

    Wire up the JSON source generator

---
+++ src/CatalogService/CatalogServiceJsonContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CatalogService;
+
+// https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/source-generation
+
+[JsonSerializable(typeof(Catalog))]
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+public partial class CatalogServiceJsonContext : JsonSerializerContext { }
--- src/CatalogService/Program.cs
+++ src/CatalogService/Program.cs
@@ -3,6 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    // Write up the JSON source generator and make sure we remove the reflection fallback.
+    // this is a good way to verify that the source generator is working as expected.
+    options.SerializerOptions.TypeInfoResolverChain.Clear();
+    options.SerializerOptions.TypeInfoResolverChain.Add(CatalogServiceJsonContext.Default);
+
+    // The consumer of this API isn't JavaScript so we don't need to escape the HTML characters.
+    options.SerializerOptions.Encoder = null;
+});
+
 builder.Services.AddDbContextPool<CatalogDbContext>(options =>
 {
     var connectionString = builder.Configuration.GetConnectionString("CatalogDb") ??
@@ -23,8 +34,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapCatalogApi();
 
