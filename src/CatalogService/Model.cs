ac8afe92e94b79c5687662c07b4d0c881142833d
David Fowler <davidfowl@gmail.com>
2023-05-15 13:58:42 -0700

    Did I fix or break it again?

--- src/CatalogService/Model.cs
+++ src/CatalogService/Model.cs
@@ -16,7 +16,7 @@ public class CatalogDbContext(DbContextOptions<CatalogDbContext> options) : DbCo
                   .OrderBy(ci => ci.Id)
                   .Where(ci => catalogBrandId == null || ci.CatalogBrandId == catalogBrandId)
                   .Where(ci => before == null || ci.Id <= before)
-                  .Where(ci => after == null || ci.Id > after)
+                  .Where(ci => after == null || ci.Id >= after)
                   .Take(pageSize + 1));
     }
 
@@ -29,7 +29,7 @@ public class CatalogDbContext(DbContextOptions<CatalogDbContext> options) : DbCo
                     .Where(ci => catalogBrandId == null || ci.CatalogBrandId == catalogBrandId)
                     // https://learn.microsoft.com/ef/core/querying/pagination#keyset-pagination
                     .Where(ci => before == null || ci.Id <= before)
-                    .Where(ci => after == null || ci.Id > after)
+                    .Where(ci => after == null || ci.Id >= after)
                     .Take(pageSize + 1)
                     .ToListAsync();
     }
@@ -51,7 +51,7 @@ public class CatalogDbContext(DbContextOptions<CatalogDbContext> options) : DbCo
             FROM "Catalog" AS c
             WHERE ({catalogBrandIdParameter} IS NULL OR c."CatalogBrandId" = {catalogBrandIdParameter})
             AND ({beforeParameter} IS NULL OR c."Id" <= {beforeParameter})
-            AND ({afterParameter} IS NULL OR c."Id" > {afterParameter})
+            AND ({afterParameter} IS NULL OR c."Id" >= {afterParameter})
             ORDER BY c."Id"
             LIMIT {pageSize + 1}
         """;
