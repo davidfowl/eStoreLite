7fde87087aebceb4f66c8a964f9a2327cf5477b4
Damian Edwards <damian@damianedwards.com>
2023-05-21 15:40:56 -0700

    Print runtime version and GC mode on start

--- src/CatalogService/Program.cs
+++ src/CatalogService/Program.cs
@@ -1,8 +1,11 @@
+using System.Runtime.InteropServices;
+using System.Runtime;
+
 var builder = WebApplication.CreateSlimBuilder(args);
 
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
-    // Write up the JSON source generator and make sure we remove the reflection fallback.
+    // Wire up the JSON source generator and make sure we remove the reflection fallback.
     // this is a good way to verify that the source generator is working as expected.
     options.SerializerOptions.TypeInfoResolverChain.Clear();
     options.SerializerOptions.TypeInfoResolverChain.Add(CatalogServiceJsonContext.Default);
@@ -18,5 +21,8 @@ var app = builder.Build();
 app.MapCatalogApi();
 
 // await app.Services.InitializeDatabaseAsync();
+
+app.Logger.LogInformation("Runtime version: {runtimeVersion}", RuntimeInformation.FrameworkDescription);
+app.Logger.LogInformation("Garbage collection mode: {gcMode}", GCSettings.IsServerGC ? "Server" : "Workstation");
 
 app.Run();
