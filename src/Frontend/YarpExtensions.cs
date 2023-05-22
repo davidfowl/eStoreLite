d952d16f62d786b51a2c8cb03ff97a7a9e09d4cf
Damian Edwards <damian@damianedwards.com>
2023-05-22 15:17:36 -0700

    Add YarpExtensions

---
+++ src/Frontend/YarpExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Yarp.ReverseProxy.Forwarder;
+using Yarp.ReverseProxy.Transforms;
+using Yarp.ReverseProxy.Transforms.Builder;
+
+namespace Frontend;
+
+public static class YarpExtensions
+{
+    // Helper to wrap some ceremony for transforming the outgoing proxied request
+    public static IEndpointConventionBuilder MapForwarder(this IEndpointRouteBuilder routes, string pattern, string destinationPrefix, string targetPath)
+    {
+        var transformBuilder = routes.ServiceProvider.GetRequiredService<ITransformBuilder>();
+
+        var transform = transformBuilder.Create(c => c.AddPathRouteValues(targetPath));
+
+        return routes.MapForwarder(pattern, destinationPrefix, new ForwarderRequestConfig(), transform);
+    }
+}
