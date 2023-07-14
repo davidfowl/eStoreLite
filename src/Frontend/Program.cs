cc5803fa0ce838bb37b582121071084e4af315e4
David Fowler <davidfowl@gmail.com>
2023-07-14 17:38:45 -0400

    It's working!

--- src/Frontend/Program.cs
+++ src/Frontend/Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Components.Endpoints;
-
 var builder = WebApplication.CreateBuilder(args);
 
 var catalogServiceUrl = builder.Configuration["CatalogServiceUrl"] ??
@@ -18,9 +16,7 @@ var app = builder.Build();
 
 app.UseStaticFiles();
 
-app.MapRazorComponents<MainLayout>();
-
-app.MapGet("/", (int? before, int? after) => new RazorComponentResult<App>(new { before, after }));
+app.MapRazorComponents<App>();
 
 app.MapForwarder("/catalog/images/{id}", catalogServiceUrl, "/api/v1/catalog/items/{id}/image");
 
